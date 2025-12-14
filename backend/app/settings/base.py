d4c3a5a75f9569df180b5ee276988a25565c89f3
Imran Hoshain <iforuimran@gmail.com>
2025-12-14 17:50:59 +0600

    Update docker and .env

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'schema_viewer',
     'drf_spectacular',
     'drf_spectacular_sidecar',
